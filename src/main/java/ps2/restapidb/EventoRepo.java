fe8bd7d4e1da89569a9991a619f79fa6c5274eb6
pedrogabriel999 <131916451+pedrogabriel999@users.noreply.github.com>
2024-05-03 17:09:25 +0000

    Projeto PS Parte 1

---
+++ src/main/java/ps2/restapidb/EventoRepo.java
@@ -0,0 +1,6 @@
+package ps2.restapidb;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EventoRepo extends CrudRepository<Evento, Long> {
+}
--- src/main/java/ps2/restapidb/_App.java
+++ src/main/java/ps2/restapidb/_App.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class _App {
 
-	public static void main(String[] args) {
-		SpringApplication.run(_App.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(_App.class, args);
+    }
 
 }
 
